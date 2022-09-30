e3899a224e4e5aa713eff1a8b2ec0838d7583f8d
RezaMarchyana203040079 <203040079@mail.unpas.ac.id>
2022-09-30 15:38:20 +0700

    mengganti warna

--- pw/index.php
+++ pw/index.php
@@ -28,39 +28,40 @@ $buku = query("SELECT * FROM buku");
   <br>
   <br>
   <br>
-  <table class="tabel" border="1" cellpadding="13" cellspacing="0">
-    <tr class="top">
-      <th>#</th>
-      <th>opsi</th>
-      <th>kode</th>
-      <th>judul</th>
-      <th>penulis</th>
-      <th>penerbit</th>
-      <th>tahun terbit</th>
-      <th>stok</th>
-      <th>gambar</th>
-    </tr>
-    <?php $i = 1; ?>
-    <?php foreach ($buku as $bu) : ?>
-      <tr class="isi">
-        <td><?= $i ?></td>
-        <td>
-          <a href="php/ubah.php?id=<?= $bu['id_buku'] ?>"><button class="ubah">Ubah</button></a>
-          <a href="php/hapus.php?id=<?= $bu['id_buku'] ?>" onclick="return confirm('Hapus data??')"><button class="hapus">Hapus</button></a>
-        </td>
-        <td><?= $bu['kode_buku']; ?></td>
-        <td><?= $bu['judul_buku']; ?></td>
-        <td><?= $bu['penulis_buku']; ?></td>
-        <td><?= $bu['penerbit_buku']; ?></td>
-        <td><?= $bu['tahun_penerbit']; ?></td>
-        <td><?= $bu['stok']; ?></td>
-        <td><img class="ukuran" src="assets/img/<?= $bu['gambar']; ?>" alt=""></td>
-
+  <div class="tabel">
+    <table border="1" cellpadding="13" cellspacing="0">
+      <tr class="top">
+        <th>#</th>
+        <th>opsi</th>
+        <th>kode</th>
+        <th>judul</th>
+        <th>penulis</th>
+        <th>penerbit</th>
+        <th>tahun terbit</th>
+        <th>stok</th>
+        <th>gambar</th>
       </tr>
-      <?php $i++; ?>
-    <?php endforeach; ?>
-  </table>
+      <?php $i = 1; ?>
+      <?php foreach ($buku as $bu) : ?>
+        <tr class="isi">
+          <td><?= $i ?></td>
+          <td>
+            <a href="php/ubah.php?id=<?= $bu['id_buku'] ?>"><button class="ubah">Ubah</button></a>
+            <a href="php/hapus.php?id=<?= $bu['id_buku'] ?>" onclick="return confirm('Hapus data??')"><button class="hapus">Hapus</button></a>
+          </td>
+          <td><?= $bu['kode_buku']; ?></td>
+          <td><?= $bu['judul_buku']; ?></td>
+          <td><?= $bu['penulis_buku']; ?></td>
+          <td><?= $bu['penerbit_buku']; ?></td>
+          <td><?= $bu['tahun_penerbit']; ?></td>
+          <td><?= $bu['stok']; ?></td>
+          <td><img class="ukuran" src="assets/img/<?= $bu['gambar']; ?>" alt=""></td>
 
+        </tr>
+        <?php $i++; ?>
+      <?php endforeach; ?>
+    </table>
+  </div>
   <footer class="page-footer blue-grey darken-4 center white-text">
     <div class="container">
       <p class="flow-text">RezaMarchyana. Copyright 2022.</p>
